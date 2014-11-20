84cc19a52ccf7a8d7124a97b0d1abd7c2e436828
Breno Thales <breno.thales@gmail.com>
2014-11-20 18:08:51 -0500

    alterando as validacoes de form

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,2 +1,4 @@
 class Form < ActiveRecord::Base
+	validates_presence_of :nome, :email, :on => :create, :message => "não pode ficar em branco"
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
